977055967dca4d4766628a16edc306bfb88fb275
Jacky-Giti <song.fuwei@giti.com>
2017-10-11 17:44:20 +0800

    1

--- IQBPay/scripts/Web/OrderList.js
+++ IQBPay/scripts/Web/OrderList.js
@@ -1,16 +1,12 @@
-﻿$(document).ready(function () {
-  //  Query();
+﻿
+$(document).ready(function () {
+    var th = $("#trHeader th").eq(0);
+    alert(th.text());
+    alert(th.css("width"));
+ //  Query(true);
 });
-function MakeTable()
-{
-    var tabel = $("#DivTable");
 
-
-
-
-}
-
-function Query() {
+function Query(NeedClearn) {
 
     var url = "/Order/Query";
     $.ajax({
@@ -19,8 +15,8 @@ function Query() {
         url: url,
         success: function (data) {
             var arrLen = data.length;
-
-            $("#trContainer").empty();
+            if (NeedClearn)
+                $("#trContainer").empty();
             if (arrLen > 0) {
                 generateData(data);
             }
@@ -34,32 +30,69 @@ function Query() {
 }
 
 function generateData(result) {
+    
     var strCtrl = "";
     $.each(result, function (i) {
+        var thWidth; 
 
         var ppRealAmt = result[i].TotalAmount - result[i].SellerCommission;
 
         strCtrl = "";
         strCtrl += "<tr>";
-   
-        strCtrl += "<td>" + result[i].OrderNo + "</td>";
-        strCtrl += "<td>" + result[i].AliPayOrderNo + "</td>";
-        strCtrl += "<td>" + result[i].TotalAmount + "</td>";
-        strCtrl += "<td>" + result[i].Rate + "</td>";
-        strCtrl += "<td>" + result[i].RateAmount + "</td>";
-        strCtrl += "<td>" + result[i].AgentName + "</td>";
-        strCtrl += "<td>" + result[i].RealTotalAmount + "</td>";
-        strCtrl += "<td>" + result[i].SellerName + "</td>";
-        strCtrl += "<td>" + result[i].SellerChannel + "</td>";
-        strCtrl += "<td>" + result[i].SellerRate + "</td>";
-        strCtrl += "<td>" + result[i].SellerCommission + "</td>";
-        strCtrl += "<td>" + ppRealAmt + "</td>";
-        strCtrl += "<td></td>";
-        strCtrl += "<td></td>";
-      
 
         strCtrl += "<td><a href='/Order/Info?id=" + result[i].OrderNo + "' class='td'>详情</a>";
         strCtrl += " <input type='hidden' value='" + result[i].FilePath + "'</td>";
+
+        tdWidth = "width:"+ $("#trHeader th").eq(1).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].OrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(2).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(3).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(4).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(5).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(6).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(7).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(8).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(9).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(10).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(11).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(12).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(13).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(14).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(15).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+
+        tdWidth = "width:" + $("#trHeader th").eq(16).css("width");
+        strCtrl += "<td style='" + tdWidth + "'>" + result[i].AliPayOrderNo + "</td>";
+      
+
+       
         strCtrl += "</tr>";
 
         $("#trContainer").append(strCtrl);
